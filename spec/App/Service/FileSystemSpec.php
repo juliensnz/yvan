3df1e966fbbe94bbdb4096c638de03977a27f2af
Arthur MILLET <arthurosmillet@gmail.com>
2018-07-25 17:14:32 +0200

    Maj Spec v7 FileSystemSpec upgrade

--- spec/App/Service/FileSystemSpec.php
+++ spec/App/Service/FileSystemSpec.php
@@ -18,23 +18,27 @@ class FileSystemSpec extends ObjectBehavior
         $this->shouldHaveType(FileSystem::class);
     }
 
-    function it_creates_directory()
+    function it_creates_directory($fileSystem)
     {
+        $fileSystem->mkdir('../workdir')->shouldBeCAlled();
         $this->createDirectory('../workdir');
     }
 
-    function it_removes_directory()
+    function it_removes_directory($fileSystem)
     {
+        $fileSystem->remove('../workdir')->shouldBeCAlled();
         $this->removeDirectory('../workdir');
     }
 
-    function it_copies_file()
+    function it_copies_file($fileSystem)
     {
+        $fileSystem->copy('../workdir/composer.lock', 'lock/pim-community-dev/composer.lock')->shouldBeCAlled();
         $this->copyFile('../workdir/composer.lock', "lock/pim-community-dev/composer.lock");
     }
 
-    function it_should_not_throw_an_copy_file_exception()
+    function it_should_not_throw_an_copy_file_exception($fileSystem)
     {
-        $this->shouldThrow('App\Exception\CopyFileException')->during('copyFile', [['../workdir/composer.lock'],["lock/pim-community-dev/composer.lock"]]);
+        $fileSystem->copy('../workdir/composer.lock', 'lock/pim-community-dev/composer.lock')->willThrow(new \Exception());
+        $this->shouldThrow('App\Exception\CopyFileException')->during('copyFile', [['../workdir/composer.lock'],['lock/pim-community-dev/composer.lock']]);
     }
 }
